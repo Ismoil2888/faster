29984929ccb74d1122427c4dccea6a66389e0096
Ismoil Hamidzoda <ismoil2888@gmail.com>
2024-08-22 22:42:42 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import './Registration.css'
+import SignUp from './components/auth/SignUp';
+import SignIn from './components/auth/SignIn';
+import AuthDetails from './components/auth/AuthDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/authdetails" element={<AuthDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import SignUp from './components/auth/SignUp';
+// import SignIn from './components/auth/SingIn';
+// import AuthDetails from './components/auth/AuthDetails';
+
+// function App() {
+//       return (
+//      <div className="App">
+//       <SignUp/>
+//       <SignIn/>
+//       <AuthDetails/>
+//      </div>
+//     );
+// }
+
+// export default App;
