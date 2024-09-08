aad5ebcdd20e66fb20fb3c3c1697bde2339fe2e4
Ismoil Hamidzoda <ismoil2888@gmail.com>
2024-09-08 17:51:25 +0500

    update 17:51

--- src/components/NotfoundPage.jsx
+++ src/components/NotfoundPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import SignUp from './auth/SignUp';
+
 
 export function NotfoundPage() {
     return (
        <div>
         <h1>Страница не найдена</h1>
+        <p>
+            <Link to="/signup">вернуться домой</Link>
+        </p>
        </div>
     )
 }
