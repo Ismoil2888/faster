ded21a3310552845c4912333d858ebd845c82ff8
Ismoil Hamidzoda <ismoil2888@gmail.com>
2024-09-15 15:32:45 +0500

    new update commit

--- src/components/NotfoundPage.jsx
+++ src/components/NotfoundPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import SignUp from './auth/SignUp';
 
 
 export function NotfoundPage() {
@@ -9,7 +8,7 @@ export function NotfoundPage() {
        <div className='notfoundpage'>
         <h1>Страница не найдена</h1>
         <p>
-            <Link className='homeback' to="/signup">Вернуться домой</Link>
+            <Link className='homeback' to="/signup">Вернуться на главную страницу</Link>
         </p>
        </div>
     </div>
